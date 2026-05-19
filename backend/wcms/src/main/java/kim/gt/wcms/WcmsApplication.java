293d1c9584b93edb4366ba453f3166b83eedea03
GTKim <kgk5309@gmail.com>
2026-05-19 13:24:10 +0900

    chore(backend): add initial Spring Boot scaffold

---
+++ backend/wcms/src/main/java/kim/gt/wcms/WcmsApplication.java
@@ -0,0 +1,13 @@
+package kim.gt.wcms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WcmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WcmsApplication.class, args);
+    }
+
+}
